3d20fb570066ec78963f2f15f88aa9851878edbd
Ксения Сентюрева <ksusha1108@yandex.ru>
2023-07-09 22:17:51 +0300

    added item 1

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -6,6 +6,9 @@ from loader import bot
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
-    bot.reply_to(
-        message, "Эхо без состояния или фильтра.\n" f"Сообщение: {message.text}"
-    )
+    if message.text.lower() == "привет":
+        bot.send_message(message.chat.id, 'привет')
+    else:
+        bot.reply_to(
+            message, "Эхо без состояния или фильтра.\n" f"Сообщение: {message.text}"
+        )
--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,5 +1,6 @@
 from telebot.types import Message
 
+
 from loader import bot
 
 
